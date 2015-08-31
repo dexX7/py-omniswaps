f139c34bc7217956ce2af6d0268da3de006723ec
dexX7 <dexx@bitwatch.co>
2015-08-31 15:05:50 +0200

    Cleanup server and remove forms

--- swap_server/app/views.py
+++ swap_server/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, jsonify
+from flask import render_template, jsonify
 from app import app
 from controller import *
 
@@ -30,29 +30,4 @@ def signed(txid, vout, scriptPubKey, redeemScript):
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form)
+    return render_template("index.html", title='Home')

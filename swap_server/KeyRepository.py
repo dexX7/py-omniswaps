53d83fa38fe7442899d6563c99713236bee6e78f
dexX7 <dexx@bitwatch.co>
2015-10-02 20:09:41 +0200

    Add KeyRepository

---
+++ swap_server/KeyRepository.py
@@ -0,0 +1,38 @@
+
+
+class KeyAlreadyUsed(Exception):
+    def __init__(self):
+        pass
+
+
+class KeyUnknown(Exception):
+    def __init__(self):
+        pass
+
+
+class InvalidScript(Exception):
+    def __init__(self):
+        pass
+
+
+class KeyRepository:
+
+    def GetNextPubKey(self):
+        """
+        Generates and stores a new key-pair.
+
+        :return: the public key corresponding to the new key
+        """
+        return NotImplemented
+
+    def SignTransaction(self, rawTx, vIns, sigHashType, signingKey):
+        """
+        Signs a raw transaction.
+
+        :param rawTx:       the raw transaction to sign
+        :param vIns:        the transaction inputs, including scriptPubKey and redeemScript
+        :param sigHashType: the signature hash type
+        :param signingKey:  the key used to sign the transaction
+        :return: the signed raw transaction
+        """
+        return NotImplemented

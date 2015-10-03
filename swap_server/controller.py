2470e0b5c2bf74c095ca026588df6e0e9c7df830
dexX7 <dexx@bitwatch.co>
2015-10-03 15:22:26 +0200

    Cleanup and remove deprechiated

--- swap_server/controller.py
+++ swap_server/controller.py
@@ -1,5 +1,6 @@
+from bitcoinrpc.authproxy import AuthServiceProxy
 from MemoryKeyRepository import MemoryKeyRepository
-from server import *
+import config
 
 
 class Controller:
@@ -13,71 +14,3 @@ class Controller:
 
     def Sign(self, rawTx, vIns, sigHashType, signingKey):
         return self.repository.SignTransaction(rawTx, vIns, sigHashType, signingKey)
-
-
-# DEPRECIATED
-def getpubkey(pubkeyhash=None):
-    if pubkeyhash is None:
-        pubkeyhash = getnewaddress()
-    result = validateaddress(pubkeyhash)
-    return result['pubkey']
-
-
-# DEPRECIATED
-def getscriptpubkey(pubkeyhash):
-    result = validateaddress(pubkeyhash)
-    return result['scriptPubKey']
-
-
-# DEPRECIATED
-def createshared(other_pubkey):
-    pubkey = getpubkey()
-    pubkeys = sorted([pubkey, other_pubkey])
-    response_create = createmultisig(len(pubkeys), pubkeys)
-    response_script = decodescript(response_create['redeemScript'])
-    addmultisigaddress(len(pubkeys), pubkeys)
-    result = {
-        'address': response_script['p2sh'],
-        'pubkeys': pubkeys,
-        'redeemScript': response_create['redeemScript'],
-        'reqSigs': response_script['reqSigs'],
-        'scriptPubKey': getscriptpubkey(response_script['p2sh']),
-        'type': response_script['type'],
-    }
-    return result
-
-
-# DEPRECIATED
-def createunsigned(txid, vout):
-    rawtx = createrawtransaction([{'txid': txid, 'vout': vout}])
-    decoded = decoderawtransaction(rawtx)
-    return {
-        "hex": rawtx,
-        'txid': decoded['txid'],
-        'version': decoded['version'],
-        'locktime': decoded['locktime'],
-        'vin': decoded['vin'],
-        'vout': decoded['vout'],
-    }
-
-
-# DEPRECIATED
-# TOOD: must not sign any other than original! request + sign must be atomic!
-def createsigned(txid, vout, scriptPubKey, redeemScript):
-    rawtx = createunsigned(txid, vout)['hex']
-    signresult = signrawtransaction(rawtx, [{
-        'txid': txid,
-        'vout': vout,
-        'scriptPubKey': scriptPubKey,
-        'redeemScript': redeemScript
-    }], 'NONE|ANYONECANPAY')
-    rawtx = signresult['hex']
-    decoded = decoderawtransaction(rawtx)
-    return {
-        'hex': rawtx,
-        'txid': decoded['txid'],
-        'version': decoded['version'],
-        'locktime': decoded['locktime'],
-        'vin': decoded['vin'],
-        'vout': decoded['vout'],
-    }

fcc88763b8f6a5cc4b61c19b2f27b8a13ff827a9
dexX7 <dexx@bitwatch.co>
2015-10-05 22:48:47 +0200

    Update user API

--- swap_user/api.py
+++ swap_user/api.py
@@ -1,6 +1,6 @@
+import json
 import requests
 import config
-
 
 def request_shared(pubkey):
     r = requests.get('%s/createshared/%s' % (config.API_ENDPOINT, pubkey))
@@ -10,4 +10,24 @@ def request_shared(pubkey):
 def request_signed(txid, vout, scriptPubKey, redeemScript):
     r = requests.get('%s/createsigned/%s-%d-%s-%s' % (
         config.API_ENDPOINT, txid, vout, scriptPubKey, redeemScript))
+    return r.json()
+
+
+def request_pubkey():
+    r = requests.get('%s/getpubkey' % (config.API_ENDPOINT_WEB))
+    return r.json()
+
+
+def request_sign(rawTx, prevTxs, sigHashType, signingKey):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'rawtx': rawTx,
+        'prevtxs': prevTxs,
+        'sighashtype': sigHashType,
+        'key': signingKey
+    }
+    url = '%s/sign' % (config.API_ENDPOINT_WEB)
+    r = requests.post(url, data=json.dumps(data), headers=headers)
     return r.json()
--- swap_user/config.py
+++ swap_user/config.py
@@ -4,3 +4,4 @@ RPC_CONNECT = '127.0.0.1'
 RPC_PORT = 18334
 
 API_ENDPOINT = 'http://127.0.0.1:5000'
+API_ENDPOINT_WEB = 'http://api.bitwatch.co:5000'

e64fb434127c90f760bc0d3a1bbf7259dee176bf
dexX7 <dexx@bitwatch.co>
2015-10-16 17:56:03 +0200

    Use regtest port

--- swap_user/config.py
+++ swap_user/config.py
@@ -1,7 +1,7 @@
 RPC_USER = 'bitcoinrpc'
 RPC_PASSWORD = 'pass'
 RPC_CONNECT = '127.0.0.1'
-RPC_PORT = 18334
+RPC_PORT = 18332
 
 API_ENDPOINT = 'http://127.0.0.1:5000'
 API_ENDPOINT_WEB = 'http://api.bitwatch.co:5000'

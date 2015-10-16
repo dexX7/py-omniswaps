e45f9cd8f508d6037be42b087b9a3331c1b455b1
dexX7 <dexx@bitwatch.co>
2015-10-16 18:01:57 +0200

    Prettify api.py

--- swap_user/api.py
+++ swap_user/api.py
@@ -2,23 +2,24 @@ import json
 import requests
 import config
 
-def request_shared(pubkey):
+
+def requestShared(pubkey):
     r = requests.get('%s/createshared/%s' % (config.API_ENDPOINT, pubkey))
     return r.json()
 
 
-def request_signed(txid, vout, scriptPubKey, redeemScript):
+def requestSigned(txid, vout, scriptPubKey, redeemScript):
     r = requests.get('%s/createsigned/%s-%d-%s-%s' % (
         config.API_ENDPOINT, txid, vout, scriptPubKey, redeemScript))
     return r.json()
 
 
-def request_pubkey():
-    r = requests.get('%s/getpubkey' % (config.API_ENDPOINT_WEB))
+def requestPubkey():
+    r = requests.get('%s/getpubkey' % config.API_ENDPOINT_WEB)
     return r.json()
 
 
-def request_sign(rawTx, prevTxs, sigHashType, signingKey):
+def requestSign(rawTx, prevTxs, sigHashType, signingKey):
     headers = {
         'Content-Type': 'application/json'
     }
@@ -28,6 +29,6 @@ def request_sign(rawTx, prevTxs, sigHashType, signingKey):
         'sighashtype': sigHashType,
         'key': signingKey
     }
-    url = '%s/sign' % (config.API_ENDPOINT_WEB)
+    url = '%s/sign' % config.API_ENDPOINT_WEB
     r = requests.post(url, data=json.dumps(data), headers=headers)
     return r.json()

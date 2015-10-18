e38be6ebaaf0bd3f233ac09fad7e6f12cc4f23d4
dexX7 <dexx@bitwatch.co>
2015-10-18 21:44:22 +0200

    Update server views

--- swap_server/app/views.py
+++ swap_server/app/views.py
@@ -2,6 +2,7 @@ from flask import make_response, jsonify, request
 from KeyRepository import KeyAlreadyUsed
 from KeyRepository import KeyUnknown
 from KeyRepository import InvalidScript
+from OrderRepository import OrderNotFound, OrderAlreadyExists
 from app import app
 from controller import Controller
 
@@ -29,6 +30,10 @@ def internal_error(error):
 
     return respond(result, code)
 
+
+##
+# Key handling:
+#
 
 @app.route('/getpubkey')
 def getpubkey():
@@ -75,6 +80,102 @@ def sign():
     except InvalidScript:
         result = {'error': 'invalid request'}
         code = 403
+
+    except:
+        result = {'error': 'internal server error'}
+        code = 500
+
+    return respond(result, code)
+
+
+##
+# Order handling:
+#
+
+@app.route('/getorder/<orderId>')
+def getorder(orderId):
+    try:
+        order = ctrl.GetOrder(orderId)
+        result = {'orderId': orderId, 'order': order}
+        code = 200
+
+    except OrderNotFound:
+        result = {'error': 'order not found'}
+        code = 404
+
+    except:
+        result = {'error': 'internal server error'}
+        code = 500
+
+    return respond(result, code)
+
+
+@app.route('/addorder', methods=['POST'])
+def addorder():
+    data = request.get_json()
+
+    if 'order' not in data:
+        result = {'error': 'malformed request'}
+        code = 400
+
+        return respond(result, code)
+
+    order = data['order']
+
+    try:
+        orderId = ctrl.AddOrder(order)
+        result = {'orderId': orderId, 'order': order}
+        code = 201
+
+    except OrderAlreadyExists:
+        result = {'error': 'order already exists'}
+        code = 409
+
+    except:
+        result = {'error': 'internal server error'}
+        code = 500
+
+    return respond(result, code)
+
+
+@app.route('/removeorder', methods=['POST'])
+def removeorder():
+    data = request.get_json()
+
+    if 'orderId' not in data:
+        result = {'error': 'malformed request'}
+        code = 400
+
+        return respond(result, code)
+
+    orderId = data['orderId']
+
+    try:
+        status = ctrl.RemoveOrder(orderId)
+        result = {'orderId': orderId, 'removed': status}
+        code = 200
+
+    except OrderNotFound:
+        result = {'error': 'order not found'}
+        code = 404
+
+    except:
+        result = {'error': 'internal server error'}
+        code = 500
+
+    return respond(result, code)
+
+
+##
+# Misc:
+#
+
+@app.route('/reset')
+def reset():
+    try:
+        ctrl.resetRepositories()
+        result = {'reset': True}
+        code = 200
 
     except:
         result = {'error': 'internal server error'}

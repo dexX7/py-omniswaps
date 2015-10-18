02d90bae5ed2db2bb80d2f1f76d1ee7c2c1a0ab4
dexX7 <dexx@bitwatch.co>
2015-10-18 22:07:25 +0200

    Update user API

--- swap_user/api.py
+++ swap_user/api.py
@@ -3,11 +3,13 @@ import requests
 import config
 
 
+# Depreciated
 def requestShared(pubkey):
     r = requests.get('%s/createshared/%s' % (config.API_ENDPOINT, pubkey))
     return r.json()
 
 
+# Depreciated
 def requestSigned(txid, vout, scriptPubKey, redeemScript):
     r = requests.get('%s/createsigned/%s-%d-%s-%s' % (
         config.API_ENDPOINT, txid, vout, scriptPubKey, redeemScript))
@@ -30,5 +32,39 @@ def requestSign(rawTx, prevTxs, sigHashType, signingKey):
         'key': signingKey
     }
     url = '%s/sign' % config.API_ENDPOINT_WEB
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    return r.json()
+
+
+def requestGetOrder(orderId):
+    r = requests.get('%s/getorder/%s' % (config.API_ENDPOINT_WEB, orderId))
+    return r.json()
+
+
+def requestGetOrders():
+    r = requests.get('%s/getorders' % config.API_ENDPOINT_WEB)
+    return r.json()
+
+
+def requestAddOrder(order):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'order': order
+    }
+    url = '%s/addorder' % config.API_ENDPOINT_WEB
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    return r.json()
+
+
+def requestRemoveOrder(orderId):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'orderId': orderId
+    }
+    url = '%s/removeorder' % config.API_ENDPOINT_WEB
     r = requests.post(url, data=json.dumps(data), headers=headers)
     return r.json()

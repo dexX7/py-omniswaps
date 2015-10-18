f0c3c115be1fb1a915224371edcc212caa5ac1b8
dexX7 <dexx@bitwatch.co>
2015-10-18 21:44:14 +0200

    Update server controller

--- swap_server/controller.py
+++ swap_server/controller.py
@@ -1,3 +1,4 @@
+from MemoryOrderRepository import MemoryOrderRepository
 from bitcoinrpc.authproxy import AuthServiceProxy
 from MemoryKeyRepository import MemoryKeyRepository
 import config
@@ -7,10 +8,26 @@ class Controller:
     def __init__(self):
         rpcServer = AuthServiceProxy(
             'http://%s:%s@%s:%d' % (config.RPC_USER, config.RPC_PASSWORD, config.RPC_CONNECT, config.RPC_PORT))
-        self.repository = MemoryKeyRepository(rpcServer)
+        self.keys = MemoryKeyRepository(rpcServer)
+        self.orders = MemoryOrderRepository(rpcServer)
 
     def GetNextPubKey(self):
-        return self.repository.GetNextPubKey()
+        return self.keys.GetNextPubKey()
 
     def Sign(self, rawTx, vIns, sigHashType, signingKey):
-        return self.repository.SignTransaction(rawTx, vIns, sigHashType, signingKey)
+        return self.keys.SignTransaction(rawTx, vIns, sigHashType, signingKey)
+
+    def GetOrder(self, orderId):
+        return self.orders.GetOrder(orderId)
+
+    def AddOrder(self, order):
+        return self.orders.AddOrder(order)
+
+    def RemoveOrder(self, orderId):
+        return self.orders.RemoveOrder(orderId)
+
+    def resetRepositories(self):
+        rpcServer = AuthServiceProxy(
+            'http://%s:%s@%s:%d' % (config.RPC_USER, config.RPC_PASSWORD, config.RPC_CONNECT, config.RPC_PORT))
+        self.keys = MemoryKeyRepository(rpcServer)
+        self.orders = MemoryOrderRepository(rpcServer)

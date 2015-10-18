b5428fc0fe3bb9247180d070144c0c5450f639a1
dexX7 <dexx@bitwatch.co>
2015-10-18 22:10:37 +0200

    Add atomic_list_orders

---
+++ swap_user/atomic_list_orders.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+
+from api import requestGetOrders
+from util import printJson
+
+
+def ListOrders():
+    """
+    Requests published orders.
+    """
+    return requestGetOrders()
+
+
+def help():
+    print('atomic_list_orders\n')
+    print('Lists currently published orders.\n')
+    print('Arguments:')
+    print('None\n')
+    print('Example:')
+    print('./atomic_list_orders.py')
+    exit()
+
+
+def main():
+    if len(sys.argv) > 1 and 'help' in str(sys.argv[1]):
+        help()
+    if len(sys.argv) != 1:
+        help()
+
+    print("\nResponse:")
+
+    result = ListOrders()
+    printJson(result)
+
+
+if __name__ == "__main__":
+    main()

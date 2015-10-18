2066a1233d73e6c7c6450dcd66db71553796a5f1
dexX7 <dexx@bitwatch.co>
2015-10-18 21:25:34 +0200

    Add OrderRepository

---
+++ swap_server/OrderRepository.py
@@ -0,0 +1,38 @@
+class OrderAlreadyExists(Exception):
+    def __init__(self):
+        pass
+
+
+class OrderNotFound(Exception):
+    def __init__(self):
+        pass
+
+
+class OrderRepository:
+
+    def GetOrder(self, orderId):
+        """
+        Returns information about an order.
+
+        :param str orderId: the order identifier
+        :returns: the order
+        """
+        return NotImplemented
+
+    def AddOrder(self, order):
+        """
+        Adds an order to the orderbook.
+
+        :param str order: the order to add
+        :returns: the order identifier
+        """
+        return NotImplemented
+
+    def RemoveOrder(self, orderId):
+        """
+        Removes an order to the orderbook.
+
+        :param str orderId: the order identifier
+        :returns: True, if the order was removed successfully
+        """
+        return NotImplemented
